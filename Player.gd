076486143eb36485b8df8f2b77bbcdb6657f01dd
PhylMacao <13609252+PhylMacao@users.noreply.github.com>
2021-05-26 19:51:28 +0200

    wrote basic player movement

---
+++ Player.gd
@@ -0,0 +1,43 @@
+extends KinematicBody2D
+
+
+# Declare member variables here.
+export (int) var speed = 120
+export (int) var jump_speed = -1800
+export (int) var gravity = 0
+export (float, 0, 1.0) var friction = 0.1
+export (float, 0, 1.0) var acceleration = 0.25
+
+
+var velocity = Vector2.ZERO
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func get_input():
+	var dir = 0
+	if Input.is_action_pressed("walk_right"):
+		$AnimatedSprite.flip_h = true
+		dir += 1
+	if Input.is_action_pressed("walk_left"):
+		$AnimatedSprite.flip_h = false
+		dir -= 1
+	if dir != 0:
+		velocity.x = lerp(velocity.x, dir * speed, acceleration)
+	else:
+		velocity.x = lerp(velocity.x, 0, friction)
+
+func _physics_process(delta):
+	get_input()
+	velocity.y += gravity * delta
+	velocity = move_and_slide(velocity, Vector2.UP)
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor():
+			velocity.y = jump_speed
